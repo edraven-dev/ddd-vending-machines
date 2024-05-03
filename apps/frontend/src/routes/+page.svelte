266ad1df19628fc9d75be19db80c692714a9154f
ArkadiuszKolodynski <ArkadiuszKolodynski@users.noreply.github.com>
2024-05-03 18:24:34 +0200

    feat: add kubernetes config

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
   import Management from '$components/management.svelte';
-  import { title } from '$lib/title';
 
   export let data;
   const { headOffice } = data;
 </script>
-
-<svelte:head>
-  <title>{title}</title>
-</svelte:head>
 
 <Management {headOffice} />
