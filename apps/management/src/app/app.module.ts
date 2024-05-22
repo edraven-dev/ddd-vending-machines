2df1545041a7819ec39cdd4e8cd0472ae779e3de
ArkadiuszKolodynski <ArkadiuszKolodynski@users.noreply.github.com>
2024-05-22 14:07:17 +0200

    feat: add logger middleware

--- apps/management/src/app/app.module.ts
+++ apps/management/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BullModule } from '@nestjs/bullmq';
-import { Global, Module, Provider } from '@nestjs/common';
+import { Global, MiddlewareConsumer, Module, NestModule, Provider } from '@nestjs/common';
 import { EventsModule } from '@vending-machines/events';
-import { APP_NAME_TOKEN, InvalidOperationExceptionFilterProvider, ValidationProvider } from '@vending-machines/shared';
+import {
+  APP_NAME_TOKEN,
+  InvalidOperationExceptionFilterProvider,
+  LoggerMiddleware,
+  ValidationProvider,
+} from '@vending-machines/shared';
 import { BullConfig } from './config/bull.config';
 import { DatabaseModule } from './database/database.module';
 import { ManagementModule } from './management/management.module';
@@ -14,4 +19,8 @@ const AppNameProvider: Provider = { provide: APP_NAME_TOKEN, useValue: 'Manageme
   providers: [AppNameProvider, ValidationProvider, InvalidOperationExceptionFilterProvider],
   exports: [AppNameProvider],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
